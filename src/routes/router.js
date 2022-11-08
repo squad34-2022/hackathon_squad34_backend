9db9c0cb2776a17a89f31e47a19f82ee8463e779
Amanda Falcão <amandaf.dias96@gmail.com>
2022-11-08 10:24:54 -0300

    :bricks: ci: Adding a required field to the user model

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,59 +1,63 @@
-const { Router } = require("express")
-const userRouter = new Router()
-const UserModel = require("../models/userModel")
+const { Router } = require("express");
+const userRouter = new Router();
+const UserModel = require("../models/userModel");
 
-userRouter.get("/", async (req,res) => {
+userRouter.get("/", async (req, res) => {
   try {
-    const allUsers = await UserModel.find()
-    res.json(allUsers)
-  } catch (err){
-    res.status(500).send("Error on application")
-    console.log(err)
-  }
-})
-
-userRouter.get("/:id", async(req, res) => {
-  const { id } = req.params
-  try {
-    const getUserById = await UserModel.findById(id)
-    res.json(getUserById)
+    const allUsers = await UserModel.find();
+    res.json(allUsers);
   } catch (err) {
-    res.status(500).send("Error on application")
+    res.status(500).send("Error on application");
+    console.log(err);
   }
-})
+});
 
-userRouter.post("/", async (req,res) => {
-  const { name, email } = req.body
+userRouter.get("/:id", async (req, res) => {
+  const { id } = req.params;
   try {
-    const newUser = await UserModel.create({ name, email })
-    res.json(newUser) 
-  } catch(err) {
-    res.status(500).send("Error on application")
-    console.log(err)
+    const getUserById = await UserModel.findById(id);
+    res.json(getUserById);
+  } catch (err) {
+    res.status(500).send("Error on application");
   }
-})
+});
 
-userRouter.put("/:id", async (req,res) => {
-  const { id } = req.params
-  const { name, email } = req.body
+userRouter.post("/", async (req, res) => {
+  const { name, email, password } = req.body;
   try {
-    const updatedUser = await UserModel.findByIdAndUpdate(id, { name, email })
-    res.json(updatedUser)
-  } catch(err) {
-    res.status(500).send("Error on application")
-    console.log(err)
+    const newUser = await UserModel.create({ name, email, password });
+    res.json(newUser);
+  } catch (err) {
+    res.status(500).send("Error on application");
+    console.log(err);
   }
-})
+});
 
-userRouter.delete("/:id", async (req,res) => {
-  const { id } = req.params
+userRouter.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, email, password } = req.body;
   try {
-    const deletedUser = await UserModel.findByIdAndDelete(id)
-    res.json(deletedUser)
-  } catch(err) {
-    res.status(500).send("Error on application")
-    console.log(err)
+    const updatedUser = await UserModel.findByIdAndUpdate(id, {
+      name,
+      email,
+      password,
+    });
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(500).send("Error on application");
+    console.log(err);
   }
-})
+});
 
-module.exports = userRouter
+userRouter.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedUser = await UserModel.findByIdAndDelete(id);
+    res.json(deletedUser);
+  } catch (err) {
+    res.status(500).send("Error on application");
+    console.log(err);
+  }
+});
+
+module.exports = userRouter;
