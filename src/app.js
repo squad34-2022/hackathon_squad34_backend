13d67d49eed26176547946accbee859692edf5da
Octavio Chagas Junior <86677802+OctavioJunior@users.noreply.github.com>
2022-11-09 18:06:25 -0300

    :sparkles: feat: back to front connection

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 require("./config/database");
 const express = require("express");
+const cors = require("cors");
 
 const { PORT } = process.env;
 
@@ -10,7 +11,12 @@ const courses = require("./controllers/course");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
+app.get("/teste", (req, res) => {
+  res.json({ message: "Conectado ao Backend" });
+});
 
 app.use("/users", users);
 app.use("/trails", trails);
